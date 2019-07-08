c92cc8ab543681454ccfb4555718a6718dd91411
czfshine <czfshine@outlook.com>
2019-07-08 15:25:25 +0800

    更新项目结构

---
+++ source/back-end/tiku/src/main/java/cn/czfshine/tiku/TikuApplication.java
@@ -0,0 +1,13 @@
+package cn.czfshine.tiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikuApplication.class, args);
+    }
+
+}
