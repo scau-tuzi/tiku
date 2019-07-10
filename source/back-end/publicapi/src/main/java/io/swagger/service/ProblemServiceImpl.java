3e9e1e311b6242014c16487b1c33cc5149094339
czfshine <czfshine@outlook.com>
2019-07-10 11:05:52 +0800

    and 和or 表达式

--- source/back-end/publicapi/src/main/java/io/swagger/service/ProblemServiceImpl.java
+++ source/back-end/publicapi/src/main/java/io/swagger/service/ProblemServiceImpl.java
@@ -4,9 +4,10 @@ import cn.czfshine.tiku.pojo.ProblemFullData;
 import io.swagger.model.Expression;
 import io.swagger.utils.Parser;
 import io.swagger.utils.ParserErrorException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ProblemServiceImpl implements ProblemService {
     @Override
     public List<ProblemFullData> queryProblem(Expression expression) throws ParserErrorException {
--- source/back-end/publicapi/src/main/java/io/swagger/utils/Parser.java
+++ source/back-end/publicapi/src/main/java/io/swagger/utils/Parser.java
@@ -9,16 +9,14 @@ import cn.czfshine.tiku.pojo.dao.repos.ProblemRepository;
 import cn.czfshine.tiku.pojo.dao.repos.ProblemTagRepository;
 import cn.czfshine.tiku.pojo.dao.repos.TagRepository;
 import io.swagger.model.Expression;
-import io.swagger.model.ProblemInfo;
+import io.swagger.service.ProblemDataService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 查询表达式解析器
@@ -34,45 +32,8 @@ public class Parser {
     * @return
     */
    public static List<ProblemFullData> getAllProblemsByExpression(Expression expression) throws ParserErrorException {
-
-      @NotNull String op = expression.getOperator();
-      @NotNull Object argument1 = expression.getArgument1();
-      if(!(argument1 instanceof String) ){
-         throw new ParserErrorException("操作【"+expression.getOperator()+"】的第一个参数类型必须是字符串，但是收到的不是");
-      }
-      switch (op){
-         case "==":{
-
-            @NotNull Object argument2 = expression.getArgument2();
-            if(!(argument2 instanceof String) ){
-               throw new ParserErrorException("操作【==】的第二个参数类型必须是字符串，但是收到的不是");
-            }
-            List<Long> res = problemEquals((String) argument1, (String) argument2);
-            break;
-         }
-         case "contains":{
-            @NotNull Object argument2 = expression.getArgument2();
-            if(!(argument2 instanceof List) ){
-               throw new ParserErrorException("操作【contains】的第二个参数类型必须是数组，但是收到的不是");
-            }
-            int size = ((List) argument2).size();
-            if(size == 0 ){
-               throw new ParserErrorException("操作【contains】的第二个参数数组为空");
-            }
-            ArrayList<String> strings = new ArrayList<>();
-            for (int i = 0; i < ((List) argument2).size(); i++) {
-               Object item = ((List) argument2).get(i);
-               if(!(item instanceof String) ){
-                  throw new ParserErrorException("操作【contains】的第二个参数数组元素必须是字符串，但是收到的不是");
-               }
-               strings.add((String)item);
-            }
-            break;
-         }
-         default:
-            throw  new ParserErrorException("操作【"+op+"】目前不被支持");
-      }
-      return new ArrayList<>();
+      List<Long> longs = executeExpression(expression);
+      return problemDataService.getFullDataByIds(longs);
    }
 
 
@@ -81,7 +42,7 @@ public class Parser {
     * @param expression
     * @return
     */
-   public static List<PaperFullData> getAllPapersByEpression(Expression expression){
+   public static List<PaperFullData> getAllPapersByExpression(Expression expression){
       return new ArrayList<>();
    }
 
@@ -162,11 +123,147 @@ public class Parser {
       return res;
    }
 
+   /**执行表达式，返回符合结果的问题id
+    * @param expression
+    * @return
+    * @throws ParserErrorException
+    */
+   private static List<Long> executeExpression(Expression expression) throws ParserErrorException {
+      @NotNull String op = expression.getOperator();
+
+
+      switch (op){
+         case "==":{
+            @NotNull Object argument1 = expression.getArgument1();
+            if(!(argument1 instanceof String) ){
+               throw new ParserErrorException("操作【"+expression.getOperator()+"】的第一个参数类型必须是字符串，但是收到的不是");
+            }
+            @NotNull Object argument2 = expression.getArgument2();
+            if(!(argument2 instanceof String) ){
+               throw new ParserErrorException("操作【==】的第二个参数类型必须是字符串，但是收到的不是");
+            }
+            List<Long> res = problemEquals((String) argument1, (String) argument2);
+            return res;
+         }
+         case "contains":{
+            @NotNull Object argument1 = expression.getArgument1();
+            if(!(argument1 instanceof String) ){
+               throw new ParserErrorException("操作【"+expression.getOperator()+"】的第一个参数类型必须是字符串，但是收到的不是");
+            }
+            @NotNull Object argument2 = expression.getArgument2();
+            if(!(argument2 instanceof List) ){
+               throw new ParserErrorException("操作【contains】的第二个参数类型必须是数组，但是收到的不是");
+            }
+            int size = ((List) argument2).size();
+            if(size == 0 ){
+               throw new ParserErrorException("操作【contains】的第二个参数数组为空");
+            }
+            ArrayList<String> strings = new ArrayList<>();
+            for (int i = 0; i < ((List) argument2).size(); i++) {
+               Object item = ((List) argument2).get(i);
+               if(!(item instanceof String) ){
+                  throw new ParserErrorException("操作【contains】的第二个参数数组元素必须是字符串，但是收到的不是");
+               }
+               strings.add((String)item);
+            }
+            List<Long> longs = problemContains((String) argument1, strings);
+            return longs;
+         }
+         case "and":{
+            @NotNull Object argument1 = expression.getArgument1();
+            if(!(argument1 instanceof LinkedHashMap) ){
+               throw new ParserErrorException("操作【"+expression.getOperator()+"】的第一个参数类型必须是json对象，但是收到的不是");
+            }
+            Expression expression1 = mapToExpression((LinkedHashMap) argument1);
+
+            @NotNull Object argument2 = expression.getArgument2();
+            if(!(argument2 instanceof LinkedHashMap) ){
+               throw new ParserErrorException("操作【"+expression.getOperator()+"】的第二个参数类型必须是json对象，但是收到的不是");
+            }
+            Expression expression2 = mapToExpression((LinkedHashMap) argument2);
+            return andOperator(expression1,expression2);
+         }
+         case "or":{
+            @NotNull Object argument1 = expression.getArgument1();
+            if(!(argument1 instanceof LinkedHashMap) ){
+               throw new ParserErrorException("操作【"+expression.getOperator()+"】的第一个参数类型必须是json对象，但是收到的不是");
+            }
+            @NotNull Object argument2 = expression.getArgument2();
+            if(!(argument2 instanceof LinkedHashMap) ){
+               throw new ParserErrorException("操作【"+expression.getOperator()+"】的第二个参数类型必须是json对象，但是收到的不是");
+            }
+            Expression expression1 = mapToExpression((LinkedHashMap) argument1);
+            Expression expression2 = mapToExpression((LinkedHashMap) argument2);
+            return orOperator(expression1,expression2);
+         }
+         default:
+            throw  new ParserErrorException("操作【"+op+"】目前不被支持");
+      }
+
+   }
+
+
+   private static Expression mapToExpression(LinkedHashMap json) throws ParserErrorException {
+      Expression expression = new Expression();
+      Object operator;
+      Object argument1;
+      Object argument2;
+
+      try{
+
+         operator = json.getOrDefault("operator",null);
+
+         if(operator==null){
+            throw  new ParserErrorException("表示式的json对象必须有operator域");
+         }
+
+         if(!(operator instanceof String )){
+            throw  new ParserErrorException("表示式的json对象的operator域必须是字符串");
+         }
+
+         argument1 = json.getOrDefault("argument1", null);
+
+         if(argument1==null){
+            throw  new ParserErrorException("表示式的json对象必须有argument1域");
+         }
+         argument2 = json.getOrDefault("argument2", null);
+
+         if(argument2==null){
+            throw  new ParserErrorException("表示式的json对象必须有argument2域");
+         }
+
+      }catch (Exception e){
+         throw  new ParserErrorException("解析表达式对象失败");
+      }
+      expression.setOperator((String) operator);
+      expression.setArgument1(argument1);
+      expression.setArgument2(argument2);
+      return expression;
+   }
+
+   private static List<Long> orOperator(Expression left,Expression right) throws ParserErrorException {
+      // todo 短路
+      List<Long> l = executeExpression(left);
+      List<Long> r = executeExpression(right);
+      HashSet<Long> ls = new HashSet<>(l);
+      ls.addAll(r);
+      return new ArrayList<>(ls);
+   }
+   private static List<Long> andOperator(Expression left,Expression right) throws ParserErrorException {
+      // todo 短路
+      List<Long> l = executeExpression(left);
+      List<Long> r = executeExpression(right);
+      HashSet<Long> ls = new HashSet<>(l);
+      return r.stream().filter(ls::contains).collect(Collectors.toList());
+   }
    @Autowired
    private static ProblemRepository problemRepository;
    @Autowired
    private static ProblemTagRepository problemTagRepository;
    @Autowired
    private static TagRepository tagRepository;
+
+   @Autowired
+   private static ProblemDataService problemDataService;
 }
 
