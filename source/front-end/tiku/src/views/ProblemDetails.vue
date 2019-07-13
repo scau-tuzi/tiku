f0345b824156487ef4f99d4cc685abf236111402
czfshine <czfshine@outlook.com>
2019-07-13 09:49:22 +0800

    更改界面组件

---
+++ source/front-end/tiku/src/views/ProblemDetails.vue
@@ -0,0 +1,24 @@
+<template>
+    <BasicTable>
+
+    </BasicTable>
+</template>
+
+<script>
+    import BasicTable from "../components/BasicTable";
+
+    export default {
+        name: "ProblemDetails",
+        components: {BasicTable},
+        data:()=>{
+            return{
+                fieldData:BasicFieldList,
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
