2c09c2e4fa8f0ae3efd769f16089a1c3eb17d37d
czfshine <czfshine@outlook.com>
2019-07-16 22:52:43 +0800

    加了个切面

---
+++ source/back-end/publicapi/src/main/java/io/swagger/aspect/ApiLogAspect.java
@@ -0,0 +1,48 @@
+package io.swagger.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * 请求日志
+ * todo 处理后写到文件或其他地方
+ */
+@Component
+@Slf4j
+@Aspect
+public class ApiLogAspect {
+    @Pointcut("execution(public * io.swagger.api..*.*(..))")
+    public void apiLog(){}
+
+    @Before("apiLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "apiLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        log.info("RESPONSE : " + ret);
+    }
+
+}
