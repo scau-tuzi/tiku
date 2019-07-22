fdf88083756c68c101cb253017f11fa2d5ebb380
czfshine <czfshine@outlook.com>
2019-07-22 21:27:48 +0800

    脚本

---
+++ source/back-end/upload.sh
@@ -0,0 +1,3 @@
+#scp ./initer/target/initer-0.0.1-SNAPSHOT.jar ubuntu@czfshine.cn:/home/ubuntu/initer.jar
+scp ./publicapi/target/tiku.publicapi-1.0-SNAPSHOT-exec.jar ubuntu@czfshine.cn:/home/ubuntu/publicapi.jar
+
