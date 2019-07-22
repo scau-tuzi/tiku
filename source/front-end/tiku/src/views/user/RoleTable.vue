f1a8c08e694f51d39ac37c48068e1cfc5eddd0b2
ycyycyycyycy <576341419@qq.com>
2019-07-22 15:26:37 +0800

    完善角色列表

--- source/front-end/tiku/src/views/user/RoleTable.vue
+++ source/front-end/tiku/src/views/user/RoleTable.vue
@@ -77,8 +77,8 @@
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false;onSubmit()">确 定</el-button>
+        <el-button @click="dialogFormVisible_add = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible_add = false;onSubmit()">确 定</el-button>
       </div>
     </el-dialog>
     <!-- 编辑角色窗口 -->
